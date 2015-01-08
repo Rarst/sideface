ed92d95a24e09ed3584a05d40497596481e1faba
Andrey Savchenko <contact@rarst.net>
2015-01-08 14:29:48 +0200

    Broke out params handling out of controllers.

--- index.php
+++ index.php
@@ -183,40 +183,46 @@ $totals_2 = 0;
  */
 $metrics = null;
 
+// param name, its type, and default value
+$params = [
+    'run'    => [ UPROFILER_STRING_PARAM, '' ],
+    'wts'    => [ UPROFILER_STRING_PARAM, '' ],
+    'symbol' => [ UPROFILER_STRING_PARAM, '' ],
+    'sort'   => [ UPROFILER_STRING_PARAM, 'wt' ], // wall time
+    'run1'   => [ UPROFILER_STRING_PARAM, '' ],
+    'run2'   => [ UPROFILER_STRING_PARAM, '' ],
+    'source' => [ UPROFILER_STRING_PARAM, 'uprofiler' ],
+    'all'    => [ UPROFILER_UINT_PARAM, 0 ],
+];
+
+// pull values of these params, and create named globals for each param
+uprofiler_param_init($params);
+
+/* reset params to be a array of variable names to values
+   by the end of this page, param should only contain values that need
+   to be preserved for the next page. unset all unwanted keys in $params.
+ */
+foreach ($params as $k => $v) {
+    $params[$k] = $$k;
+    // unset key from params that are using default values. So URLs aren't
+    // ridiculously long.
+    if ($params[$k] == $v[1]) {
+        unset( $params[$k] );
+    }
+}
+
+$vbar   = ' class="vbar"';
+$vwbar  = ' class="vwbar"';
+$vwlbar = ' class="vwlbar"';
+$vbbar  = ' class="vbbar"';
+$vrbar  = ' class="vrbar"';
+$vgbar  = ' class="vgbar"';
+
 $app = new Application();
 
 $app->get('/', function () {
 
     global $params;
-
-    // param name, its type, and default value
-    $params = [
-        'run'    => [ UPROFILER_STRING_PARAM, '' ],
-        'wts'    => [ UPROFILER_STRING_PARAM, '' ],
-        'symbol' => [ UPROFILER_STRING_PARAM, '' ],
-        'sort'   => [ UPROFILER_STRING_PARAM, 'wt' ], // wall time
-        'run1'   => [ UPROFILER_STRING_PARAM, '' ],
-        'run2'   => [ UPROFILER_STRING_PARAM, '' ],
-        'source' => [ UPROFILER_STRING_PARAM, 'uprofiler' ],
-        'all'    => [ UPROFILER_UINT_PARAM, 0 ],
-    ];
-
-    // pull values of these params, and create named globals for each param
-    uprofiler_param_init($params);
-
-    /* reset params to be a array of variable names to values
-       by the end of this page, param should only contain values that need
-       to be preserved for the next page. unset all unwanted keys in $params.
-     */
-    foreach ($params as $k => $v) {
-        $params[$k] = $$k;
-
-        // unset key from params that are using default values. So URLs aren't
-        // ridiculously long.
-        if ($params[$k] == $v[1]) {
-            unset( $params[$k] );
-        }
-    }
 
     ob_start();
 
@@ -227,13 +233,6 @@ $app->get('/', function () {
     echo "</head>";
 
     echo "<body>";
-
-    $vbar   = ' class="vbar"';
-    $vwbar  = ' class="vwbar"';
-    $vwlbar = ' class="vwlbar"';
-    $vbbar  = ' class="vbbar"';
-    $vrbar  = ' class="vrbar"';
-    $vgbar  = ' class="vgbar"';
 
     $uprofiler_runs_impl = new UprofilerRuns_Default();
 
@@ -260,20 +259,8 @@ $app->get('/{source}/{run}', function ($source, $run) {
 
     global $params;
 
-    // param name, its type, and default value
-    $params = [
-        'run'    => [ UPROFILER_STRING_PARAM, '' ],
-        'wts'    => [ UPROFILER_STRING_PARAM, '' ],
-        'symbol' => [ UPROFILER_STRING_PARAM, '' ],
-        'sort'   => [ UPROFILER_STRING_PARAM, 'wt' ], // wall time
-        'run1'   => [ UPROFILER_STRING_PARAM, '' ],
-        'run2'   => [ UPROFILER_STRING_PARAM, '' ],
-        'source' => [ UPROFILER_STRING_PARAM, 'uprofiler' ],
-        'all'    => [ UPROFILER_UINT_PARAM, 0 ],
-    ];
+    ob_start();
 
-    // pull values of these params, and create named globals for each param
-    uprofiler_param_init($params);
     echo "<html>";
 
     echo "<head><title>uprofiler: Hierarchical Profiler Report</title>";
